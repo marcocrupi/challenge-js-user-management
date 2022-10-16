97f95d3fd2f68ac886e951eb6908ccd3fe67930b
DarioCorbinelli <108214071+DarioCorbinelli@users.noreply.github.com>
2022-10-16 19:15:16 +0200

    fix: login page - final adjustments

--- login.js
+++ login.js
@@ -1,20 +1,30 @@
-document.querySelector("#submitLogin").addEventListener("click", async e => {
-  e.preventDefault()
-  
-  const response = await fetch("https://api-nodejs-todolist.herokuapp.com/user/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      email: document.querySelector("#emailLogin").value,
-      password: document.querySelector("#passwordLogin").value
-    })
-  })
+document.querySelector("#submitLogin").addEventListener("click", async (e) => {
+  e.preventDefault();
 
-  // salva token solo se account viene trovato
-  if(response.ok) {
-    const data = await response.json()
-    sessionStorage.setItem("token", data.token)
+  try {
+    const response = await fetch(
+      "https://api-nodejs-todolist.herokuapp.com/user/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: document.querySelector("#emailLogin").value,
+          password: document.querySelector("#passwordLogin").value,
+        }),
+      }
+    );
+    
+    if (response.ok) {
+      const data = await response.json();
+      sessionStorage.setItem("token", data.token);
+      window.open("profile.html");
+    } else {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+
+  } catch (error) {
+    console.error(`Catch: ${error}`);
   }
-})
+});
