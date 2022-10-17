6e08a4159a653afd16fad82610e685b1c21e5b98
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-10-17 11:10:41 +0200

    data inject to DOM v.1

--- profile.js
+++ profile.js
@@ -1,1 +1,22 @@
 
+
+//Data
+const data = [{"email":"mail@mail","fullName":"Geggio Geggi","age":24}]; //per ora dummy
+
+//function
+function injectHtml() {
+  return data.map((item) => {
+      const {email,fullName,age} = item;
+      //HTML
+      return `
+      <div class="block">
+        <p class="email">${email}</p>
+        <p class="fullName">${fullName}</p>
+        <p class="age">${age}</p>
+      </div>
+      `;
+    });
+}
+
+//DOM
+document.getElementById("divParent").insertAdjacentHTML("beforeend", injectHtml(data));
