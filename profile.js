181a7f6ce1f23c05f0b661da15a381e17cd7a8d0
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-10-18 14:23:47 +0200

    aggiunta versione per soli array dati

--- profile.js
+++ profile.js
@@ -4,19 +4,34 @@
 const data = [{"email":"mail@mail","fullName":"Geggio Geggi","age":24}]; //per ora dummy
 
 //function
-function injectHtml() {
-  return data.map((item) => {
-      const {email,fullName,age} = item;
-      //HTML
-      return `
-      <div class="block">
-        <p class="email">${email}</p>
-        <p class="fullName">${fullName}</p>
-        <p class="age">${age}</p>
-      </div>
-      `;
-    });
-}
+// function injectHtml() {
+//   return data.map((item) => {
+//       const {email,fullName,age} = item;
+//       //HTML
+//       return `
+//       <div class="Dati">
+//         <p class="email">${email}</p>
+//         <p class="fullName">${fullName}</p>
+//         <p class="age">${age}</p>
+//       </div>
+//       `;
+//     });
+// }
 
-//DOM
-document.getElementById("divParent").insertAdjacentHTML("beforeend", injectHtml(data));
+// //DOM
+// document.getElementById("divParent").insertAdjacentHTML("beforeend", injectHtml(data));
+
+
+// // versione per object, non funzionante perchè?
+async function injectHtml(){
+  const {email, fullName, age} = data;
+  const container = document.getElementById("divParent");
+  container.innerHTML=`
+  <div class="Dati">
+  <p class="email">${email}</p>
+  <p class="fullName">${fullName}</p>
+  <p class="age">${age}</p>
+  </div>
+  `;  
+  }
+injectHtml()
