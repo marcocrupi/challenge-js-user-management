ff77588ff32b2486747f16b1bf98361be90828ae
Christian <christianlestingi33@gmail.com>
2022-10-18 23:52:18 +0200

    add Authorization con il Bearer Token

--- profile.js
+++ profile.js
@@ -1,4 +1,20 @@
+async()=>{
+    const response= await fetch(
+        "https://api-nodejs-todolist.herokuapp.com/user/me",
+        {
+            method: "GET",
+            headers: { Authentication: "Bearer"+ token}
+        }
+    )
 
+if(response.ok){
+    const data=await response.json();
+    sessionStorage.getItem("token",data.token);
+}
+else{
+    throw new Error(`HTTP error: ${response.status}`);
+}
+}
 
 //Data
 const data = [{"email":"mail@mail","fullName":"Geggio Geggi","age":24}]; //per ora dummy
@@ -33,6 +49,6 @@ document.getElementById("divParent").insertAdjacentHTML("beforeend", injectHtml(
 //   <p class="fullName">${fullName}</p>
 //   <p class="age">${age}</p>
 //   </div>
-//   `;  
+//   `;
 //   }
 // injectHtml()
