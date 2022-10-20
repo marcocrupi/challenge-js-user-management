3e4b6c839c1fe28d5c0ff1f6f7f5f29a8ad1d716
Marco Crupi <marcocrupi@hotmail.it>
2022-10-20 10:15:32 +0200

    add impose min max on age registration

--- registration.js
+++ registration.js
@@ -60,3 +60,14 @@ document.querySelector("#ageRegistration").addEventListener("keypress", (e) => {
     e.preventDefault();
   }
 });
+
+function imposeMinMax(el) {
+  if (el.value != "") {
+    if (parseInt(el.value) < parseInt(el.min)) {
+      el.value = el.min;
+    }
+    if (parseInt(el.value) > parseInt(el.max)) {
+      el.value = el.max;
+    }
+  }
+}
